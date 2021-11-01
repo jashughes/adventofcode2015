07ce9a2dbea07226de47b8215c838e399edae0d7
jashughes <jasmine.hughes@berkeley.edu>
2021-10-31 22:10:36 -0700

    white space and method chaining

--- 02.js
+++ 02.js
@@ -1,16 +1,19 @@
 // read and parse input
 var fs = require("fs");
-const input = fs.readFileSync('02.txt', 'utf8').toString().split("\n");
+const input = fs.readFileSync('02.txt', 'utf8')
+        .toString()
+        .split("\n");
 const low2high = (a, b) => a - b;
 const sum = (a, b) => a + b;
-const sorted = input.map(v => v.split("x")).map((v) => v.sort(low2high));
-
+const sorted = input.map(v => v.split("x"))
+        .map((v) => v.sort(low2high));
 
 // part 1
-
-var area = sorted.map(([a, b, c]) => 2 * (a*b + b*c + a*c) + a * b).reduce(sum)
+var area = sorted.map(([a, b, c]) => 2 * (a*b + b*c + a*c) + a * b)
+        .reduce(sum)
 console.log("Part 1:", area);
 
 // part 2
-var ribbon = sorted.map(([a, b, c]) => a * b * c + 2 * a + 2 * b).reduce(sum)
+var ribbon = sorted.map(([a, b, c]) => a * b * c + 2 * a + 2 * b)
+        .reduce(sum)
 console.log("Part 2:", ribbon)
