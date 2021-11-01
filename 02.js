4bc4feeb1df1f0c6a3e0a155880c8a9eb6a69908
jashughes <jasmine.hughes@berkeley.edu>
2021-10-31 22:01:12 -0700

    sort once, measure twice.

--- 02.js
+++ 02.js
@@ -1,37 +1,16 @@
-// read input
+// read and parse input
 var fs = require("fs");
-const input = fs.readFileSync('02.txt', 'utf8').toString().split("\n")
-const dims = input.map(v => v.split("x").map((v) => Number(v)))
+const input = fs.readFileSync('02.txt', 'utf8').toString().split("\n");
+const low2high = (a, b) => a - b;
+const sum = (a, b) => a + b;
+const sorted = input.map(v => v.split("x")).map((v) => v.sort(low2high));
+
 
 // part 1
-function calc_leeway(d){
-    d.sort(function(a, b){return a - b});
-    return d[0] * d[1];
-}
 
-function calc_main(d){
-    return 2*(d[0]*d[1] + d[1]*d[2] + d[2]*d[0])
-}
-
-function wrap_needs(d){
-    return calc_main(d) + calc_leeway(d)
-}
+var area = sorted.map(([a, b, c]) => 2 * (a*b + b*c + a*c) + a * b).reduce(sum)
+console.log("Part 1:", area);
 
 // part 2
-function calc_bow(d){
-    return d.reduce((a, b) => a * b)
-}
-
-function calc_around(d){
-    d.sort(function(a, b){return a - b});
-    return 2 * d[0] + 2 * d[1]
-}
-
-function ribbon_needs(d){
-    return calc_bow(d) + calc_around(d)
-}
-
-var area = dims.map(v => wrap_needs(v)).reduce((a, b) => a + b);
-var ribbon = dims.map(v => ribbon_needs(v)).reduce((a, b) => a + b);
-console.log("Part 1:", area);
+var ribbon = sorted.map(([a, b, c]) => a * b * c + 2 * a + 2 * b).reduce(sum)
 console.log("Part 2:", ribbon)
