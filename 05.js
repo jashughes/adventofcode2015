cd3213e75494209c81ffa942abf5dadf326a0edf
jashughes <jasmine.hughes@berkeley.edu>
2021-11-06 13:44:56 -0700

    part 1 day 5 in js

---
+++ 05.js
@@ -0,0 +1,15 @@
+// read input
+const fs = require("fs");
+const input = fs.readFileSync('05.txt', 'utf8').split("\n");
+
+// part 1
+const nice1 = a => a.replace(/[^aeiou]+/g,"").length >= 3;
+const nice2 = a => a.match(/([a-zA-Z0-9])\1{1}/) !== null;
+const nice3 = a => a.match(/(ab)|(cd)|(pq)|(xy)/) === null;
+
+console.log(
+  "Part 1:", 
+  input
+    .map(v => nice1(v) && nice2(v) && nice3(v))
+    .reduce((a, b) => a + b, 0)
+)
