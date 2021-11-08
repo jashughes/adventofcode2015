608543b7f679b74bb510d7c8788cbd4cbaa80d68
jashughes <jasmine.hughes@berkeley.edu>
2021-11-07 21:17:02 -0800

    minor fixes

--- 06.js
+++ 06.js
@@ -8,12 +8,12 @@ const sw = (d, v) => d === "toggle" ? Math.abs(v - 1) : d === "turn on" ? 1 : 0;
 var lights1 = Array.from(Array(1000), () => Array(1000).fill(0));
 
 // part 2
-const knob = (d, v) => d === "toggle" ? v + 2 : d === "turn on" ? v + 1 : v <= 0 ? 0: v - 1;
+const knob = (d, v) => d === "toggle" ? v + 2 : d === "turn on" ? v + 1 : Math.max(0, v - 1);
 var lights2 = Array.from(Array(1000), () => Array(1000).fill(0));
 
 input.forEach(e => {
-  for (let x = Number(e[2]); x < Number(e[4]) + 1; x++) {
-    for (let y = Number(e[3]); y < Number(e[5]) + 1; y++) {
+  for (let x = Number(e[2]); x <= Number(e[4]); x++) {
+    for (let y = Number(e[3]); y <= Number(e[5]); y++) {
       lights1[x][y] = sw(e[1], lights1[x][y]);
       lights2[x][y] = knob(e[1], lights2[x][y]);
     }
@@ -25,11 +25,11 @@ console.log(
   lights1
     .map(v => v.reduce((a, b) => a + b, 0))
     .reduce((a, b) => a + b, 0)
-)
+);
 
 console.log(
   "Part 2:", 
   lights2
     .map(v => v.reduce((a, b) => a + b, 0))
     .reduce((a, b) => a + b, 0)
-)
+);
